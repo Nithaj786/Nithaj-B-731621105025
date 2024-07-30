7527d9690326bd360a43d7f91ef1616c6fdaed5d
Yashwanth <yashwanth.n.130304@gmail.com>
2024-07-30 21:35:14 +0530

    srcangularl2

---
+++ Angular/day2/src/app/nithaj/nithaj.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nithaj',
+  templateUrl: './nithaj.component.html',
+  styleUrl: './nithaj.component.css'
+})
+export class NithajComponent {
+
+}
